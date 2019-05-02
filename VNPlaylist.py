c01d466b80335069346456448644b102cada5e34
lethanhday <trungtamnghenhin@gmail.com>
2019-05-02 11:01:00 +0700

    Update VNPlaylist.py

--- VNPlaylist.py
+++ VNPlaylist.py
@@ -1088,12 +1088,12 @@ def GetFShareCred():
 def LoginOKNoti(user="",lvl=""):
 	header = "CHÍNH THỨC RA MẮT QUỸ KHỞI NGHIỆP ASANZO STARTUP FUND"
 	message = ""
-	xbmc.executebuiltin('Notification("{}", "{}","{}", "")'.format(header, message, 20000, ''))
+	xbmc.executebuiltin('Notification("{}", "{}","{}", "")'.format(header, message, 50000, ''))
 	
 def LoginOKNoti1(user="",lvl=""):
 	header = "Hà Nội, ngày 18 tháng 04 năm 2019, Tập đoàn ASANZO chính thức ra mắt quỹ khởi nghiệp ASANZO Startup Fund trong khuôn khổ chương trình Công Bố Đối Tác Chiến Lược của Shark Tank – Thương Vụ Bạc Tỷ mùa 3 nhằm mục tiêu khuyến khích, hỗ trợ và giúp đỡ cho các dự án khởi nghiệp của người trẻ ở nước ta."
 	message = ""
-	xbmc.executebuiltin('Notification("{}", "{}","{}", "")'.format(header, message, 50000, ''))
+	xbmc.executebuiltin('Notification("{}", "{}","{}", "")'.format(header, message, 20000, ''))
 
 
 def GetFShareUser(cred):
