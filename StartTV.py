739c027f36063fa5bc02725a25e4898304510caf
lethanhday <trungtamnghenhin@gmail.com>
2019-06-16 20:35:26 +0700

    Update StartTV.py

--- StartTV.py
+++ StartTV.py
@@ -1,2 +1,4 @@
-import xbmc
-xbmc.executebuiltin("Skin.ResetSettings")
+# import the XBMC libraries so we can use the controls and functions of XBMC
+import xbmc, xbmcgui
+
+xbmc.executebuiltin(kodi-send -a 'PlayMedia(pvr://channels/tv/All channels/pvr.hts_456169418.pvr)')
