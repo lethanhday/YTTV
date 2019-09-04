08188e4d173dfb871954abe16a73ba5826f3a69e
lethanhday <trungtamnghenhin@gmail.com>
2019-09-04 10:58:05 +0700

    Update DiamondPlaylist.py

--- DiamondPlaylist.py
+++ DiamondPlaylist.py
@@ -25,8 +25,8 @@ addons_folder = xbmc.translatePath('special://home/addons')
 image = xbmc.translatePath(os.path.join(path, "icon.png"))
 
 plugin = Plugin()
-addon          = xbmcaddon.Addon("plugin.video.diamond.vn")
-pluginrootpath = "plugin://plugin.video.diamond.vn"
+addon          = xbmcaddon.Addon("plugin.video.vietmediaF")
+pluginrootpath = "plugin://plugin.video.vietmediaF"
 http = httplib2.Http(cache, disable_ssl_certificate_validation=True)
 query_url = "https://docs.google.com/spreadsheets/d/{sid}/gviz/tq?gid={gid}&headers=1&tq={tq}"
 sheet_headers = {
@@ -178,9 +178,9 @@ def getItems(url_path="0", tq="select A,B,C,D,E"):
 		if "plugin://" in item["path"]:
 			if "install-repo" in item["path"]:
 				item["is_playable"] = False
-			elif re.search("plugin.video.diamond.vn/(.+?)/.+?\://", item["path"]):
+			elif re.search("plugin.video.vietmediaF/(.+?)/.+?\://", item["path"]):
 				match = re.search(
-					"plugin.video.diamond.vn(/.+?/).+?\://", item["path"])
+					"plugin.video.vietmediaF(/.+?/).+?\://", item["path"])
 				tmp = item["path"].split(match.group(1))
 				tmp[-1] = urllib.quote_plus(tmp[-1])
 				item["path"] = match.group(1).join(tmp)
@@ -697,7 +697,7 @@ def AddTracking(items):
 	'''
 
 	for item in items:
-		if "plugin.video.diamond.vn" in item["path"]:
+		if "plugin.video.vietmediaF" in item["path"]:
 			tmps = item["path"].split("?")
 			if len(tmps) == 1:
 				tail = ""
